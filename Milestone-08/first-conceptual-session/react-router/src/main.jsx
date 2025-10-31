9be2b4416e87245d143cdc94b3b9b1ac5b31929c
suronjit02 <suronjit02@gmail.com>
2025-11-01 02:03:21 +0600

    react router consep session

--- Milestone-08/first-conceptual-session/react-router/src/main.jsx
+++ Milestone-08/first-conceptual-session/react-router/src/main.jsx
@@ -3,8 +3,33 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import About from './Pages/About.jsx';
+import Contact from './Pages/Contact.jsx';
+import Home from './Pages/Home.jsx';
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    Component: Home
+  },
+
+  {
+    path: '/about',
+    Component: About
+  },
+
+  {
+    path: '/contact',
+    Component: Contact
+  }
+
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
