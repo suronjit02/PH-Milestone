32da6dab2befb17c27bd7e610dddaf2ffc170b93
suronjit02 <suronjit02@gmail.com>
2025-11-08 23:16:49 +0600

    loading component create

---
+++ Milestone-08/first-conceptual-session/react-router/src/Components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-gray-500 font-bold'>Loading...</h1>
+        </div>
+    );
+};
+
+export default Loading;
