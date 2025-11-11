5b5c290d11b556a560912e87b412398ebca274fb
suronjit02 <suronjit02@gmail.com>
2025-11-12 02:25:49 +0600

    github authentication

--- Milestone-09/simple-dimple-auth/src/App.jsx
+++ Milestone-09/simple-dimple-auth/src/App.jsx
@@ -1,13 +1,68 @@
 import "./App.css";
 
-import { GoogleAuthProvider } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "./firebase.init";
+import { useState } from "react";
+
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 function App() {
+  
+  const [user, setUser] = useState(null);
+
+  const handleSignInGoogle = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+    })
+  };
+
+  const handleSignInGitHub = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        console.log(result.user);
+        setUser(result.user)
+      })
+      .catch(error => {
+        console.log(error);
+    })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch(error => {
+        console.log(error);
+    })
+  }
+
   return (
     <>
       <h1>Simple Dimple</h1>
 
-      <button>Sign In With Google</button>
+      {user ? 
+        <button onClick={handleSignOut}>Sign Out</button>
+       : <>
+          <button onClick={handleSignInGoogle}>Sign In With Google</button>
+          
+          <button onClick={handleSignInGitHub}>Sign In With GitHub</button>
+          </>
+      }
+
+      {user && (
+        <div>
+          <h2>{user.displayName}</h2>
+          <h4>{user.email}</h4>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </>
   );
 }
