fd8742c95f0cd1ac6343387e877229662b921b49
suronjit02 <suronjit02@gmail.com>
2025-11-12 18:36:24 +0600

    basic layout for eamil-password-auth

--- Milestone-09/email-password-auth/src/main.jsx
+++ Milestone-09/email-password-auth/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import RootLayout from './layouts/RootLayout.jsx';
-import Home from './Pages/Home.jsx';
+import RootLayout from "./layouts/RootLayout.jsx";
+import Home from "./Pages/Home.jsx";
+import Register from "./components/register/Register.jsx";
+import Login from "./components/Login/Login.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,12 +17,20 @@ const router = createBrowserRouter([
         index: true,
         Component: Home,
       },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
     ],
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
