2d6e5d34156a7e3aeffc8ebecce87e6851f81afd
suronjit02 <suronjit02@gmail.com>
2025-11-12 12:57:23 +0600

    start email-password-auth project

---
+++ Milestone-09/email-password-auth/src/layouts/RootLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const RootLayout = () => {
+    return (
+        <div>
+
+            <Navbar></Navbar>
+
+            <Outlet></Outlet>
+
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default RootLayout;
---
+++ Milestone-09/email-password-auth/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './layouts/RootLayout.jsx';
+import Home from './Pages/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
