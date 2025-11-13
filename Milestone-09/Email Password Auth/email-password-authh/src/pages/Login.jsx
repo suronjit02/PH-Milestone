ff705dbdca1c476141f1b4a724528b2b93729eb9
suronjit02 <suronjit02@gmail.com>
2025-11-14 02:43:59 +0600

    module 48 full practice project

--- Milestone-09/Email Password Auth/email-password-authh/src/pages/Login.jsx
+++ Milestone-09/Email Password Auth/email-password-authh/src/pages/Login.jsx
@@ -1,11 +1,89 @@
-import React from 'react';
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import React, { useRef, useState } from "react";
+import { Link } from "react-router";
+import { auth } from "../firebase/firebase.init";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
+  const [error, setError] = useState("");
+  const emailRef = useRef();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    //   console.log("click", email, password);
+
+    setError("");
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+
+        if (!result.user.emailVerified) {
+          alert("Please verified your email.");
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setError(error.message);
+      });
+  };
+
+  const handleForgotPassword = () => {
+    console.log("forgot password", emailRef.current.value);
+    const email = emailRef.current.value;
+      sendPasswordResetEmail(auth, email)
+          .then(() => {
+          alert('Please check yor email for reset password.')
+          })
+      
+  };
+
+  return (
+    <>
+      <form onSubmit={handleLogin}>
+        <fieldset className="fieldset m-auto mt-20 bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Login</legend>
+
+          <label className="label">Email</label>
+          <input
+            type="email"
+            ref={emailRef}
+            className="input"
+            name="email"
+            placeholder="Email"
+          />
+
+          <label className="label">Password</label>
+          <input
+            type="password"
+            className="input"
+            name="password"
+            placeholder="Password"
+          />
+          <div onClick={handleForgotPassword}>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+
+      {error && <p className="text-red-500 text-center">{error}</p>}
+
+      <p className="text-center">
+        You have account? Please{" "}
+        <Link className="text-blue-400 underline" to={"/register"}>
+          Register
+        </Link>
+      </p>
+    </>
+  );
 };
 
 export default Login;
--- Milestone-09/Email Password Auth/email-password-authh/src/pages/Register.jsx
+++ Milestone-09/Email Password Auth/email-password-authh/src/pages/Register.jsx
@@ -1,7 +1,12 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../firebase/firebase.init";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { Link } from "react-router";
 // import { auth } from "../firebase/firebase.init";
 
 const Register = () => {
@@ -13,6 +18,11 @@ const Register = () => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
+    const terms = event.target.terms.checked;
+    const name = event.target.name.value;
+    const photo = event.target.photoURL.value;
+
+    console.log(email, password, terms, name, photo);
 
     const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
     if (!passwordPattern.test(password)) {
@@ -26,11 +36,31 @@ const Register = () => {
     setError("");
     setSuccess(false);
 
+    if (!terms) {
+      setError("Please accept our terms and condition.");
+      return;
+    }
+
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         console.log(result.user);
 
         setSuccess(true);
+
+        //   profile update
+        const profile = {
+          displayName: name,
+          photoURL: photo,
+        };
+        updateProfile(result.user, profile)
+          .then(() => {})
+          .catch();
+
+        //   send email verification
+        sendEmailVerification(result.user).then(() => {
+          alert("Email verification sent!");
+        });
+
         event.target.reset();
       })
       .catch((error) => {
@@ -55,12 +85,29 @@ const Register = () => {
           <div className="card-body">
             <form onSubmit={handleRegister}>
               <fieldset className="fieldset">
+                {/* Name here */}
+                <label className="label">Full Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  className="input"
+                  placeholder="Full Name"
+                />
+                {/* email here */}
                 <label className="label">Email</label>
                 <input
                   type="email"
                   name="email"
                   className="input"
                   placeholder="Email"
+                />
+                {/* photo url here */}
+                <label className="label">Photo Url</label>
+                <input
+                  type="text"
+                  name="photoURL"
+                  className="input"
+                  placeholder="Photo Url"
                 />
                 <label className="label">Password</label>
 
@@ -77,6 +124,16 @@ const Register = () => {
                   >
                     {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
                   </button>
+                  <div>
+                    <label className="label mt-2">
+                      <input
+                        type="checkbox"
+                        name="terms"
+                        className="checkbox"
+                      />
+                      Accept Our Terms & Condition.
+                    </label>
+                  </div>
                 </div>
                 <div>
                   <a className="link link-hover">Forgot password?</a>
@@ -89,6 +146,13 @@ const Register = () => {
               )}
               {error && <p className="text-red-500">{error}</p>}
             </form>
+
+            <p>
+              Already have an account? Please{" "}
+              <Link className="text-blue-400 underline" to={"/login"}>
+                Login
+              </Link>
+            </p>
           </div>
         </div>
       </div>
