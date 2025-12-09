3a6826da7e958801f17c394ab340ee6c5640b731
suronjit02 <suronjit02@gmail.com>
2025-12-10 00:35:51 +0600

    backend setup

---
+++ Milestone-11/project/backend/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://missionscic11:rfkjddkpwVYrjkJb@cluster0.gotmti8.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      
+      
+      // Send a ping to confirm a successful connection
+      
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+      
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Mission SCIC");
+});
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
